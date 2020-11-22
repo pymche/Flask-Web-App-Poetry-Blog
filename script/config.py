b9aa333f92de74b910e62ca3b410e06c6b719fe8
Melanie <cpy.melanie@gmail.com>
2020-11-22 16:23:19 +0000

    commit

--- script/config.py
+++ script/config.py
@@ -1,7 +1,5 @@
 import os
 
-os.environ['DATABASE_URL'] = 'postgres://ftywwlawtrzyfg:ec8163dc63c7ed95af1c707525adb5fd234dd88a7331a393c6995e31efd9a2ef@ec2-176-34-114-78.eu-west-1.compute.amazonaws.com:5432/dekiit0jntesar'
-
 class Config:
-    SECRET_KEY = '4ed80220c43e95bc68fccd65816fa731'
+    SECRET_KEY = os.environ.get('SECRET_KEY')
     DATABASE_URL = os.environ['DATABASE_URL']
