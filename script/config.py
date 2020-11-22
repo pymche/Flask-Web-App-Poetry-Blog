3708d292f334f6cf8156e7f84617d9e0802c070d
Melanie <cpy.melanie@gmail.com>
2020-11-22 13:52:48 +0000

    database url change

--- script/config.py
+++ script/config.py
@@ -2,4 +2,4 @@ import os
 
 class Config:
     SECRET_KEY = '4ed80220c43e95bc68fccd65816fa731'
-    SQLALCHEMY_DATABASE_URI = 'postgresql://localhost/poetry_db'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://localhost:5432/poetry_db'
